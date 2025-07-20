0d0012b8cc0a85aa32770f8662bf20e893474d5a
Anton <anton_sima@mail.ru>
2025-07-20 23:32:48 +0500

    Создан ViewSet для курса

---
+++ materials/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Название курса')
+    preview_image = models.ImageField(upload_to='course_previews/', verbose_name='Превью курса', blank=True, null=True)
+    description = models.TextField(verbose_name='Описание курса')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Название урока')
+    description = models.TextField(verbose_name='Описание урока')
+    preview_image = models.ImageField(upload_to='lesson_previews/', verbose_name='Превью урока', blank=True, null=True)
+    video_url = models.CharField(max_length=150, verbose_name='Ссылка на видео')
---
+++ materials/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from rest_framework import viewsets
+
+from materials.models import Course
+from materials.serializers import CourseSerializer
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    serializer_class = CourseSerializer
+    queryset = Course.objects.all()
+    
