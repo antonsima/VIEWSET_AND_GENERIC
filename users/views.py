c7e1f718f3f6d2a9cc208192edf17233f2bcee87
Anton <anton_sima@mail.ru>
2025-07-23 00:00:37 +0500

    Выполнено 4 задание домашки

--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 
+from users.filters import PaymentsFilter
 from users.models import Payments
 from users.serializers import PaymentsSerializer
 
@@ -7,3 +9,5 @@ from users.serializers import PaymentsSerializer
 class PaymentsViewSet(viewsets.ModelViewSet):
     serializer_class = PaymentsSerializer
     queryset = Payments.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = PaymentsFilter
