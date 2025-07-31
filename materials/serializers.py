8ee115cef59e70c9e540bb161440208685c81f8e
Anton <anton_sima@mail.ru>
2025-07-31 22:42:35 +0500

    Выполнено 1 задание

--- materials/serializers.py
+++ materials/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from materials.models import Course, Lesson
+from materials.validators import VideoURLValidator
 
 
 class LessonSerializer(serializers.ModelSerializer):
@@ -8,6 +9,7 @@ class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = "__all__"
+        validators = [VideoURLValidator(field='video_url')]
 
 
 class CourseSerializer(serializers.ModelSerializer):
---
+++ materials/validators.py
@@ -0,0 +1,16 @@
+import re
+
+from rest_framework.serializers import ValidationError
+
+
+class VideoURLValidator:
+
+    def __init__(self, field):
+        self.field = field
+
+    def __call__(self, value):
+        reg = re.compile('youtube.com\S+')
+        tmp_val = dict(value).get(self.field)
+
+        if not bool(reg.match(tmp_val)):
+            raise ValidationError('URL видео должен вести на сайт "youtube.com"')
